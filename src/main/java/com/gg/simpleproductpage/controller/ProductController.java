d253f26bbe3f5c6e24f01ba724c85e48200f5efe
ataurcan <ataurcan@gmail.com>
2022-04-21 17:58:50 +0300

    update1

--- src/main/java/com/gg/simpleproductpage/controller/ProductController.java
+++ src/main/java/com/gg/simpleproductpage/controller/ProductController.java
@@ -3,24 +3,26 @@ package com.gg.simpleproductpage.controller;
 import com.gg.simpleproductpage.model.Product;
 import com.gg.simpleproductpage.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api")
 public class ProductController {
-
+	
+	
     @Autowired
     ProductService productService;
-
+   
+    @CrossOrigin(origins = "http://172.18.0.2:3000")
     @GetMapping(value = "/products")
     public List<Product> getProducts(){
         return productService.getProducts();
     }
-
+    @CrossOrigin(origins = "http://172.18.0.2:3000")
     @GetMapping(value = "/products/{id}")
     public List<Product> getProductById(@PathVariable int id){
         return productService.getProductById(id);
